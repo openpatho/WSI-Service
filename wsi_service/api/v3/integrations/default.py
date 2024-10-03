91d7a34aa6fb87b5be29d94a234c8109184ef5ce
martin <martin.klefas@hotmail.com>
2024-10-03 13:05:50 +0100

    Added auth logging to default profile for testing

--- wsi_service/api/v3/integrations/default.py
+++ wsi_service/api/v3/integrations/default.py
@@ -1,3 +1,4 @@
+import os, time, pickle
 class Default:
     def __init__(self, settings, logger, http_client):
         self.settings = settings
@@ -8,4 +9,25 @@ class Default:
         ...
 
     async def allow_access_slide(self, auth_payload, slide_id, manager, plugin, slide=None):
-        ...
+        # Gather data into a dictionary
+        data = {
+            "auth_payload": repr(auth_payload),
+            "slide_id": slide_id,
+            "plugin": repr(plugin),
+            "slide": repr(slide)
+        }
+
+        # Get the `data_dir` from settings and create a subfolder for the auth data
+        data_dir = os.path.join(self.settings.data_dir, "auth_logs")
+        os.makedirs(data_dir, exist_ok=True)
+
+        # Create a filename using Unix time with hundredths of a second
+        timestamp = time.time()  # Get the Unix timestamp
+        filename = os.path.join(data_dir, f"auth_data_{timestamp:.3f}.pkl")
+
+        # Pickle the data and save it to the file
+        with open(filename, 'wb') as f:
+            pickle.dump(data, f)
+
+        # Allow access for now
+        return True
