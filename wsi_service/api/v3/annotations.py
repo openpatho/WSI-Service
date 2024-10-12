c31935aec83cbbfba80a0fff780c21d08074413f
martin <martin.klefas@hotmail.com>
2024-10-12 23:04:42 +0100

    now serving basic annotations

--- wsi_service/api/v3/annotations.py
+++ wsi_service/api/v3/annotations.py
@@ -1,7 +1,29 @@
+from typing import List, Optional
+import asyncio
+
 from fastapi import Path, Depends, Header
-from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse
+
+import httpx
+import json
+
+from pathlib import Path
 
 from .singletons import api_integration
+
+from wsi_service.models.v3.slide import SlideInfo
+
+from wsi_service.custom_models.queries import (
+    ImageChannelQuery,
+    ImageFormatsQuery,
+    ImagePaddingColorQuery,
+    ImageQualityQuery,
+    PluginQuery,
+    ZStackQuery, SlideQuery,
+)
+
+async def get_authorization_header(authorization: Optional[str] = Header(None)):
+    return authorization
 
 def add_routes_annotations(app, settings, slide_manager):
     @app.get(
@@ -13,10 +35,28 @@ def add_routes_annotations(app, settings, slide_manager):
                 "content": {"application/json": {}}
             }
         },
+        description="Accepts the main slide image's ID and returns all of the annotations in 'Native' JSON format",
         tags=["Main Routes"]
     )
-    async def _(slide_id: str = SlideQuery, payload: Optional[str] = Depends(get_authorization_header)):
+    async def _(slide_id: str = SlideQuery, plugin: str = PluginQuery, skip_cache: bool = False, payload: Optional[str] = Depends(get_authorization_header)):
         """
-        Get the path to the file on the server for a slide, given its ID
+        Fetch the annotations in Native format for the specified slide
         """
-        path = await slide_manager.get_slide_file_paths(slide_id)
+        slide = await slide_manager.get_slide_info(slide_id, slide_info_model=SlideInfo, plugin=plugin)
+        await api_integration.allow_access_slide(calling_function="/slides/info",auth_payload=payload, slide_id=slide_id, manager=slide_manager,
+                                                 plugin=plugin, slide=slide)
+        
+        
+        fileNames = await slide_manager.get_slide_file_paths(slide_id)
+        anoPath = Path(fileNames[0]).with_suffix(".json")
+        
+        if anoPath.exists() and not skip_cache:
+            return FileResponse(path=str(anoPath), filename="annotations.json", media_type="application/json")
+        else:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(f"{settings.annotation_api}?slide_id={slide_id}")  # Replace with the actual URL
+                json_output = response.json()  # Parse the JSON output
+        
+            with open(str(anoPath), "w") as file:
+                json.dump(json_output, file)
+            return FileResponse(path=str(anoPath), filename="annotations.json", media_type="application/json")
--- wsi_service/settings.py
+++ wsi_service/settings.py
@@ -41,6 +41,9 @@ class Settings(BaseSettings):
     client_id: str = ""
     jwks_url: str = ""
 
+    # Anotations Endpoint Settings:
+    annotation_api: str = ""
+
     # prod mode to disable docs & enable auth
     prod_mode: bool = False
     
